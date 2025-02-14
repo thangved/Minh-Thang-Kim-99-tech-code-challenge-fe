25c1ccd1e79853f6686649ec94aaf04f045e6e7a
Kim Minh Thang <thangved02@gmail.com>
2025-02-14 11:23:14 +0700

    feat: problem 3

--- src/problem3/problem.tsx
+++ src/problem3/problem.tsx
@@ -1,5 +1,14 @@
+// FIXED: I think the currency can be static in a long time, so I think we can use enum
+enum Currency {
+  Osmosis = "Osmosis",
+  Ethereum = "Ethereum",
+  Arbitrum = "Arbitrum",
+  Zilliqa = "Zilliqa",
+  Neo = "Neo",
+}
+
 interface WalletBalance {
-  currency: string;
+  currency: Currency;
   amount: number;
 }
 interface FormattedWalletBalance {
@@ -8,71 +17,108 @@ interface FormattedWalletBalance {
   formatted: string;
 }
 
+// Define UseWalletBalances interface
+interface UseWalletBalances {
+  (): WalletBalance[];
+}
+
+const useWalletBalances: UseWalletBalances = () => {
+  return [];
+};
+
+// Define UsePrices interface
+interface PricesMap extends Record<Currency, number> {}
+interface UsePrices {
+  (): PricesMap;
+}
+const usePrices: UsePrices = () => {
+  return {
+    [Currency.Osmosis]: 0,
+    [Currency.Ethereum]: 0,
+    [Currency.Arbitrum]: 0,
+    [Currency.Zilliqa]: 0,
+    [Currency.Neo]: 0,
+  };
+};
+
+// FIXED: Should create an map to store the priority of currency instead of use switch case
+interface PriorityMap extends Record<Currency, number> {}
+const priorityMap: PriorityMap = {
+  [Currency.Osmosis]: 100,
+  [Currency.Ethereum]: 50,
+  [Currency.Arbitrum]: 30,
+  [Currency.Zilliqa]: 20,
+  [Currency.Neo]: 20,
+};
+
+// FIXED: Should move functions that not depend on props inside the component to the top level
+const getPriority = (currency: Currency): number => {
+  if (!Object.keys(priorityMap).includes(currency)) {
+    return -99;
+  }
+  return priorityMap[currency];
+};
+
 interface Props extends BoxProps {}
 const WalletPage: React.FC<Props> = (props: Props) => {
   const { children, ...rest } = props;
   const balances = useWalletBalances();
   const prices = usePrices();
 
-  const getPriority = (blockchain: any): number => {
-    switch (blockchain) {
-      case "Osmosis":
-        return 100;
-      case "Ethereum":
-        return 50;
-      case "Arbitrum":
-        return 30;
-      case "Zilliqa":
-        return 20;
-      case "Neo":
-        return 20;
-      default:
-        return -99;
-    }
-  };
-
   const sortedBalances = useMemo(() => {
     return balances
       .filter((balance: WalletBalance) => {
-        const balancePriority = getPriority(balance.blockchain);
-        if (lhsPriority > -99) {
-          if (balance.amount <= 0) {
-            return true;
-          }
-        }
+        // FIXED: maybe the correct property name is "currency" instead of "blockchain"
+        const balancePriority = getPriority(balance.currency);
+        // FIXED: incorrect variable name, apply early return
+        if (balancePriority <= -99) return false;
+        if (balance.amount <= 0) return true;
         return false;
       })
       .sort((lhs: WalletBalance, rhs: WalletBalance) => {
-        const leftPriority = getPriority(lhs.blockchain);
-        const rightPriority = getPriority(rhs.blockchain);
+        const leftPriority = getPriority(lhs.currency);
+        const rightPriority = getPriority(rhs.currency);
         if (leftPriority > rightPriority) {
           return -1;
-        } else if (rightPriority > leftPriority) {
+        }
+        // FIXED: else is unnecessary because of the return statement above
+        if (rightPriority > leftPriority) {
           return 1;
         }
+        // FIXED: should return 0 instead of undefined
+        return 0;
       });
   }, [balances, prices]);
 
-  const formattedBalances = sortedBalances.map((balance: WalletBalance) => {
-    return {
-      ...balance,
-      formatted: balance.amount.toFixed(),
-    };
-  });
+  // FIXED: should use useMemo to memoize the result
+  // FIXED: type should be specified at declaration time rather than cast it at use time.
+  const formattedBalances: FormattedWalletBalance[] = useMemo(
+    () =>
+      sortedBalances.map((balance: WalletBalance) => {
+        return {
+          ...balance,
+          formatted: balance.amount.toFixed(),
+        };
+      }),
+    [sortedBalances]
+  );
 
-  const rows = sortedBalances.map(
-    (balance: FormattedWalletBalance, index: number) => {
-      const usdValue = prices[balance.currency] * balance.amount;
-      return (
-        <WalletRow
-          className={classes.row}
-          key={index}
-          amount={balance.amount}
-          usdValue={usdValue}
-          formattedAmount={balance.formatted}
-        />
-      );
-    }
+  // FIXED: should use formattedBalances instead of sortedBalances and use useMemo to memoize the result
+  const rows = useMemo(
+    () =>
+      formattedBalances.map((balance, index: number) => {
+        const usdValue = prices[balance.currency] * balance.amount;
+        return (
+          <WalletRow
+            className={classes.row}
+            key={index}
+            amount={balance.amount}
+            usdValue={usdValue}
+            formattedAmount={balance.formatted}
+          />
+        );
+      }),
+    [formattedBalances, prices]
   );
 
   return <div {...rest}>{rows}</div>;
